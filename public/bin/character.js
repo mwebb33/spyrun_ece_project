0d726faa1ba0e3578a15b4a826deab2969246e62
Mike Webb <mikewebb@wireless-10-146-135-30.public.utexas.edu>
2014-12-08 03:35:18 -0600

    Added new movement logic

--- public/bin/character.js
+++ public/bin/character.js
@@ -1,30 +1,30 @@
-var Character = function(characterSprite) {
-
-	this.sprite = characterSprite;
-	this.sprite.buttonMode = true;
-	this.sprite.interactive = true;
+var Character = function(container) {
+	this.charSprite = container;
+	this.charSprite.pivot = new PIXI.Point(32,32);
+	this.charSprite.buttonMode = true;
+	this.charSprite.interactive = true;
 };
 
 Character.prototype.updateSprite = function(characterSprite) {
-	this.sprite = characterSprite;
+	this.charSprite = characterSprite;
 };
 
 Character.prototype.setPosition = function(x, y) {
-		this.sprite.position.x = x;
-		this.sprite.position.y = y;
+		this.charSprite.position.x = x;
+		this.charSprite.position.y = y;
 };
 
 Character.prototype.translation = function(xAmount, yAmount) {
 
 	/* Get the rectangle object that defines our coordinates */
-	var bounds = this.sprite.getLocalBounds();
+	var bounds = this.charSprite.getLocalBounds();
 
 	/* Let the Game Board detect the entire rectangle for collision */
-	var collisionDetected = gameBoard.detectCollision(this.sprite.position.x + xAmount, this.sprite.position.y + yAmount, bounds.width, bounds.height);
+	var collisionDetected = gameBoard.detectCollision(this.charSprite.position.x + xAmount, this.charSprite.position.y + yAmount, bounds.width, bounds.height);
 
 	/* Only move if no collision was detected */
 	if(collisionDetected == 0) {
-		this.setPosition(this.sprite.position.x + xAmount, this.sprite.position.y + yAmount);
+		this.setPosition(this.charSprite.position.x + xAmount, this.charSprite.position.y + yAmount);
 	}
 
 	else if(collisionDetected == 2) {
@@ -38,24 +38,81 @@ Character.prototype.translation = function(xAmount, yAmount) {
 		score.position.x = 350;
 		score.position.y = 200;
 		gameContainer.addChild(score);
+
 		//this.setPosition(125,95);
 		//this.setPosition(125,95);
 	}
 };
 
+Character.prototype.rotateCardinal = function(dirLeft,dirRight,dirDown,dirUp) {
+
+	/* Only move if no collision was detected */
+	if(dirRight) {
+		this.charSprite.rotation = 0;
+	}
+	else if(dirUp) {
+		this.charSprite.rotation = (3*Math.PI)/2;
+	}
+	else if(dirLeft) {
+		this.charSprite.rotation = Math.PI;
+	}
+	else if (dirDown) {
+		this.charSprite.rotation = Math.PI/2;
+	}
+
+	/* Only move if no collision was detected */
+	if(dirRight && dirUp) {
+		this.charSprite.rotation = (3*Math.PI)/2 + Math.PI/4;
+	}
+	else if(dirLeft && dirUp) {
+		this.charSprite.rotation = Math.PI + Math.PI/4;
+	}
+	else if(dirLeft && dirDown) {
+		this.charSprite.rotation = Math.PI - Math.PI/4;
+	}
+	else if(dirRight && dirDown) {
+		this.charSprite.rotation = 0 + Math.PI/4;
+	}
+};
+
 Character.prototype.getPosition_x = function() {
-	return this.sprite.position.x;
+	return this.charSprite.position.x;
 };
 
 Character.prototype.getPosition_y = function() {
-	return this.sprite.position.y;
+	return this.charSprite.position.y;
 };
 
 Character.prototype.updatePosition = function() {
 
-	if(key.isDown(key.LEFT)) this.translation(-2,0);
-	if(key.isDown(key.UP)) this.translation(0,-2);
-	if(key.isDown(key.DOWN)) this.translation(0,2);
-	if(key.isDown(key.RIGHT)) this.translation(2,0);
+	this.rotateCardinal(key.isDown(key.LEFT),key.isDown(key.RIGHT),
+		key.isDown(key.DOWN),key.isDown(key.UP));
 
+	this.updateAnimationState(key.isDown(key.LEFT),key.isDown(key.RIGHT),
+		key.isDown(key.DOWN),key.isDown(key.UP));
+
+	if(key.isDown(key.LEFT)){
+		this.translation(-2,0);
+	}
+	if(key.isDown(key.UP)) {
+		this.translation(0,-2);
+	}
+	if(key.isDown(key.DOWN)) {
+		this.translation(0,2);
+	}
+	if(key.isDown(key.RIGHT)) {
+		this.translation(2,0);
+	}
 };
+
+Character.prototype.updateAnimationState = function(dirLeft,dirRight,dirDown,dirUp) {
+
+	if(dirRight || dirLeft || dirDown || dirUp)
+	{
+		this.charSprite.children[0].playing = true;
+	} 
+	else 
+	{
+		this.charSprite.children[0].playing = false;
+	}
+}
